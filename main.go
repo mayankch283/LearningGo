993b5908db5bc06df66f1ee6b2bb6d18ed89ad53
mayankch283 <mayankch283@gmail.com>
2024-01-29 22:43:15 +0530

    Learned slices, arrays and for loops

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
-	cray()
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	// for i := 0; i < len(cards); i++ {
+	// 	fmt.Println(cards[i])
+	// }
 
 }
 
 func newCard() string {
-	return "5 of Diamonds"
+	return "Five of Diamonds"
 }
